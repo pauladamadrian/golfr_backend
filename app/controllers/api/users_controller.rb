9460b7617bc64077e2e86097a413a36b8247b916
Paul Adam <adrian.adam@takeofflabs.com>
2022-06-02 08:57:45 +0300

    Add route and show action for golfer's profile page

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -3,6 +3,26 @@ module Api
   class UsersController < ApplicationController
     include Devise::Controllers::Helpers
 
+    before_action :logged_in!, only: [:show]
+    before_action :correct_user, only: [:show]
+
+    def show
+      user = User.find(params[:id])
+      if user.blank?
+        render json: {
+          errors: [
+            'User not found!'
+          ]
+        }, status: :not_found
+        return
+      end
+
+      render json: {
+        name: user.name,
+        scores: user.scores.order(played_at: :desc)
+      }.to_json
+    end
+
     def login
       user = User.find_by('lower(email) = ?', params[:email])
 
@@ -25,6 +45,23 @@ module Api
           token: current_token
         }
       }.to_json
+    end
+
+    private
+
+    def current_user?(user)
+      user && user == current_user  # OR user&. == current_user
+    end
+
+    def correct_user
+      user = User.find(params[:id])
+      return if current_user?(user)
+
+      render json: {
+        errors: [
+          'You do not have access!'
+        ]
+      }, status: :unauthorized
     end
   end
 end
